e6aa1ab0df9f63f090f1a39fdb453bb7e882e35a
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-02-15 11:42:29 -0600

    added form for song name, click event button

--- src/components/UserInputSong.vue
+++ src/components/UserInputSong.vue
@@ -3,13 +3,17 @@
 </script>
 
 <template>
-  <main class="song-input">
+  <div>
+    <h3> Let's add a song to the PLaylist!</h3>
+    <input type="text" v-model="song" placeholder="Enter song name" />
+    <button @click="addSongToPlaylist">&#9835 Add Song &#9835</button>
+  </div> 
     
 
     
 
 
-  </main>
+  
  
 
 </template>
