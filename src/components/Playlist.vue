beddb5447e9718a59514e04e5709f822e775b4fb
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-02-17 17:45:20 -0600

    Added a remove song button and added some stuyling

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,14 +1,31 @@
 <script setup>
+import { defineProps} from 'vue';
+
+const props =defineProps({
+  playlist: Array,
+  onSongClick: Function
+});
+
+const emit = defineEmits();
+
+const removeSong = (songBeingRemoved) => {
+  emit('removeSong', songBeingRemoved);
+};
+
 
 </script>
 
 <template>
   <main class="playlist">
-    
-
-    
-
-
+    <h2>Playlist</h2>
+    <ul>
+      <li v-for="(song, index) in props.playlist" :key="index" @click="props.onSongClick(song)">
+        <span @click="props.onSongClick(song)">
+          {{song}}
+        </span>
+       <button @click.stop="removeSong(song)">Remove Song</button>
+      </li>
+    </ul>  
   </main>
  
 
