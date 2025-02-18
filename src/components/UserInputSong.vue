b442bfbb4492bcfa4ac32ae6547dcab89bfac20e
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-02-17 18:49:24 -0600

    added input text box for artist

--- src/components/UserInputSong.vue
+++ src/components/UserInputSong.vue
@@ -17,6 +17,7 @@ const addSongToPlaylist = () => {
 <template>
   <div>
     <h3> Let's add a song to the PLaylist!</h3>
+    <input type="text" v-model="artist" placeholder="Enter Artist name" />
     <input type="text" v-model="song" placeholder="Enter song name" />
     <button @click="addSongToPlaylist">&#9835 Add Song &#9835</button>
   </div> 
