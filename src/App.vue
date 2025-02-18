8991c837f0d45d9a1e45638bd553bb7a4e41c712
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-02-17 19:15:19 -0600

    added more text and styling

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@
     <section class="song-playing">
       <h3>Now Playing</h3>
       <p v-if="nowPlaying"> &#9835 Currently Playing &#9835 : {{nowPlaying.song}} by {{nowPlaying.artist}}</p>
-      <p v-else>Nothing is playing right now...</p>
+      <p v-else>Nothing is playing right now... ʕ•ᴥ•ʔ ??</p>
     </section>
 
     <section class="song-count">
